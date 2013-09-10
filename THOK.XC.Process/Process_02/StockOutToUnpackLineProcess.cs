50ded6fe8027688af4f7d844bda5937158918d5a
shjstudy <shjstudy@163.com>
2013-09-10 08:40:33 +0800

    二楼条码校验

--- THOK.XC.Process/Process_02/StockOutToUnpackLineProcess.cs
+++ THOK.XC.Process/Process_02/StockOutToUnpackLineProcess.cs
@@ -17,7 +17,6 @@ namespace THOK.XC.Process.Process_02
            */
 
             string[] StationState = new string[2];
-            string cigaretteCode = "";
             try
             {
                
@@ -27,41 +26,5 @@ namespace THOK.XC.Process.Process_02
                 Logger.Error("入库任务请求批次生成处理失败，原因：" + e.Message);
             }
         }
-
-        private DataTable dtCarInfo(string str)
-        {
-            //根据位置获取小车信息，位置小于当前位置的，加上总长度。按照 位置 desc 排序
-            return null;
-        }
-
-        private string GetCarNo()
-        {
-            DataTable dt = dtCarInfo("");//已经按照位置顺序排序
-            for (int i = 0; i < dt.Rows.Count; i++)
-            {
-                bool blnB = false;//true：忙碌，false：空闲
-                int Curd=0;//当前申请小车位置
-                int intd;//位置
-                if (blnB)
-                {
-                    intd = 0;//目的地
-                }
-                else
-                {
-                    intd = 0;//当前位置
-                }
-                if (Math.Abs(Curd - intd) > 100)
-                {
-                    //获取当前小车
-                    break;
-                }
-
-                
-                 
-            }
-            return "";
-        }
-
-        
     }
 }
